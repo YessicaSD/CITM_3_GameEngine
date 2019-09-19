cf0e289e1fb5e0b0607ddf0dd5a1bd4168c975a9
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2019-09-19 22:57:41 +0200

    adding imgui

--- Code/ModuleGui.cpp
+++ Code/ModuleGui.cpp
@@ -1,7 +1,76 @@
 #include "ModuleGui.h"
+#include "Application.h"
+
+
+#include "imgui\imgui_impl_opengl2.h"
+#include "SDL\include\SDL_opengl.h"
+#include "imgui\imgui_impl_sdl.h"
+
 
 ModuleGUI::ModuleGUI(Application* app, bool start_enabled):Module(app, start_enabled)
 {
 
 
+}
+
+bool ModuleGUI::Start()
+{
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+   //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	 // Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	// Setup Platform/Renderer bindings
+	ImGui_ImplSDL2_InitForOpenGL(App->window->window, App->renderer3D->context);
+	ImGui_ImplOpenGL2_Init();
+
+	
+
+
+	return true;
+}
+
+update_status ModuleGUI::Update(float dt)
+{
+	// Start the Dear ImGui frame
+	ImGui_ImplOpenGL2_NewFrame();
+	ImGui_ImplSDL2_NewFrame(App->window->window);
+	ImGui::NewFrame();
+
+	// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+	if (show_demo_window)
+		ImGui::ShowDemoWindow(&show_demo_window);
+
+
+	// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+	{
+		static float f = 0.0f;
+		static int counter = 0;
+
+		ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+		ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+		ImGui::Checkbox("Another Window", &show_another_window);
+
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+		ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+		if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+			counter++;
+		ImGui::SameLine();
+		ImGui::Text("counter = %d", counter);
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::End();
+	}
+
+	return UPDATE_CONTINUE;
+	
 }
--- Code/ModuleGui.h
+++ Code/ModuleGui.h
@@ -1,11 +1,19 @@
 #ifndef MODULEGUI_H_
 #define MODULEGUI_H_
 #include "Module.h"
+#include "imgui\imgui.h"
 
 class ModuleGUI : public Module
 {
 public:
 	ModuleGUI(Application* app, bool start_enabled = true);
+	bool Start() override;
+
+	// Our state
+	bool show_demo_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	update_status Update(float dt) override;
 	
 };
 #endif // !MODULEGUI_H_
