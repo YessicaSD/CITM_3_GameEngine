e563c0ddde44a27e4457adcecce460b64b50e5d2
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2019-09-22 16:33:28 +0200

    adding MathGeoLib

--- Code/ModuleScene01.cpp
+++ Code/ModuleScene01.cpp
@@ -1,7 +1,13 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModuleScene01.h"
-#include "Primitive.h"
+
+#include "MathGeoLib\include\MathGeoLib.h"
+#include "MathGeoLib\include\MathGeoLibFwd.h"
+#include "MathGeoLib\include\MathBuildConfig.h"
+#include "MathGeoLib\include\Math\float3.h"
+
+#include "MathGeoLib\include\Geometry\Sphere.h"
 
 ModuleScene01::ModuleScene01(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -32,10 +38,13 @@ bool ModuleScene01::CleanUp()
 // Update: draw background
 update_status ModuleScene01::Update(float dt)
 {
-	Plane p(0, 1, 0, 0);
-	p.axis = true;
-	p.Render();
-
+	//Plane p(0, 1, 0, 0);
+	//p.axis = true;
+	//p.Render();
+	Sphere sphere1;
+	sphere1.pos = { 0,0,0 };
+	sphere1.r = 5;
+	
 	return UPDATE_CONTINUE;
 }
 
