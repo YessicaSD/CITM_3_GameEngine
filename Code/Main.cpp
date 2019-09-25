77349ead876ec211257a32ce01be8ffe82b5ae87
JaumeMontagut <jaume.montagut.i.guix@gmail.com>
2019-09-25 11:22:41 +0200

    glew libraries excluded from the gitignore
    they were under a folder named Release, so they weren't being commited

--- Code/Main.cpp
+++ Code/Main.cpp
@@ -5,8 +5,8 @@
 #include "SDL/include/SDL.h"
 #pragma comment( lib, "SDL/libx86/SDL2.lib" )
 #pragma comment( lib, "SDL/libx86/SDL2main.lib" )
-#pragma comment (lib, "glew/lib/Release/Win32/glew32.lib")
-#pragma comment (lib, "glew/lib/Release/Win32/glew32s.lib")
+#pragma comment (lib, "glew/lib/glew32.lib")
+#pragma comment (lib, "glew/lib/glew32s.lib")
 #pragma comment (lib, "opengl32.lib") /* link Microsoft OpenGL lib   */
 
 enum main_states
