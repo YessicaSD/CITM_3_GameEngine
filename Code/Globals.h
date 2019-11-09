fd7ba8bbd0828fbde2df5ca1ef4f349ccc5fa926
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2019-11-09 23:04:34 +0100

    solved bug with texture

--- Code/Globals.h
+++ Code/Globals.h
@@ -61,7 +61,7 @@ enum update_status
     }
 
 //Configuration
-#define ASSETS_FOLDER "/Assets/"
+#define ASSETS_FOLDER "Assets/"
 #define SETTINGS_FOLDER "/Settings/"
 #define LIBRARY_FOLDER "/Library/"
 #define LIBRARY_AUDIO_FOLDER "/Library/Audio/"
