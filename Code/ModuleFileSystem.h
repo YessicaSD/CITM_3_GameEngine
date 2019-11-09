38be619f91e950601a2fead103ae044e472170cd
jaume <jaume.montagut.i.guix@gmail.com>
2019-11-09 16:30:50 +0100

    compiling

--- Code/ModuleFileSystem.h
+++ Code/ModuleFileSystem.h
@@ -8,5 +8,5 @@ public:
 	ModuleFileSystem(const char * name);
 	void GetExtension(const char* full_path, std::string& extension);
 	//uint Save(const char * file, const void * buffer, unsigned int size, bool append);
-
+};
 #endif // !MODULE_FILESYSTEM_H_
