6abfe888b8e0be8460ed2674259e1f4a18b71b01
JaumeMontagut <jaume.montagut.i.guix@gmail.com>
2019-11-13 10:02:23 +0100

    creating functions to copy data

--- Code/Resource.cpp
+++ Code/Resource.cpp
@@ -4,3 +4,14 @@ UID Resource::GetUID() const
 {
 	return uid;
 }
+
+void GetFileData(void ** info, void ** data_cursor, size_t bytes)
+{
+	memcpy(*data_cursor, *info, bytes);
+	data_cursor += bytes;
+}
+
+void SetFileData(void * data, void * cursor, size_t bytes)
+{
+
+}
