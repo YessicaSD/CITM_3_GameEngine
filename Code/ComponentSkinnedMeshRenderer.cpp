4d5594f1a46a7643e4162f6cbb73deafe55fd9b3
jaume <jaume.montagut.i.guix@gmail.com>
2019-12-27 15:56:00 +0100

    doing animation as explained in class
    we needed to pass the transformation onto the mesh to be able to use the local matrix and make use of the transformation hirearchy.

--- Code/ComponentSkinnedMeshRenderer.cpp
+++ Code/ComponentSkinnedMeshRenderer.cpp
@@ -96,7 +96,7 @@ void ComponentSkinnedMeshRenderer::OnUpdate(float dt)
 					}
 				}
 
-				float4x4 trans = gameobject->transform->GetGlobalMatrix().Inverted() * bone->GetGlobalMatrix();
+				float4x4 trans = gameobject->transform->GetLocalMatrix().Inverted() * bone->GetGlobalMatrix();
 				trans = trans *  resource_bone->offset_matrix;
 
 				if (resource_bone != nullptr)
